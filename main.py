e57e15641323c7d92ebc951ffd3d47c1043afaea
Andrei R Stamate <a144185i@gmail.com>
2025-04-14 22:28:47 +0300

    prettify league names

--- main.py
+++ main.py
@@ -27,6 +27,54 @@ os.makedirs(CACHE_FOLDER, exist_ok=True)
 
 CONFIG_FILE = "config.json"
 
+LEAGUE_NAMES = {
+    "soccer_argentina_primera_division": "Argentina Primera Division",
+    "soccer_australia_aleague": "Australia A-League",
+    "soccer_austria_bundesliga": "Austria Bundesliga",
+    "soccer_belgium_first_div": "Belgium First Division",
+    "soccer_brazil_campeonato": "Brazil Campeonato",
+    "soccer_brazil_serie_b": "Brazil Serie B",
+    "soccer_chile_campeonato": "Chile Campeonato",
+    "soccer_china_superleague": "China Super League",
+    "soccer_conmebol_copa_libertadores": "CONMEBOL Copa Libertadores",
+    "soccer_conmebol_copa_sudamericana": "CONMEBOL Copa Sudamericana",
+    "soccer_denmark_superliga": "Denmark Superliga",
+    "soccer_efl_champ": "EFL Championship",
+    "soccer_england_league1": "England League One",
+    "soccer_england_league2": "England League Two",
+    "soccer_epl": "English Premier League",
+    "soccer_fa_cup": "FA Cup",
+    "soccer_finland_veikkausliiga": "Finland Veikkausliiga",
+    "soccer_france_ligue_one": "France Ligue 1",
+    "soccer_france_ligue_two": "France Ligue 2",
+    "soccer_germany_bundesliga": "Germany Bundesliga",
+    "soccer_germany_bundesliga2": "Germany Bundesliga 2",
+    "soccer_germany_liga3": "Germany Liga 3",
+    "soccer_greece_super_league": "Greece Super League",
+    "soccer_italy_serie_a": "Italy Serie A",
+    "soccer_italy_serie_b": "Italy Serie B",
+    "soccer_japan_j_league": "Japan J-League",
+    "soccer_korea_kleague1": "Korea K-League 1",
+    "soccer_league_of_ireland": "League of Ireland",
+    "soccer_mexico_ligamx": "Mexico Liga MX",
+    "soccer_netherlands_eredivisie": "Netherlands Eredivisie",
+    "soccer_norway_eliteserien": "Norway Eliteserien",
+    "soccer_poland_ekstraklasa": "Poland Ekstraklasa",
+    "soccer_portugal_primeira_liga": "Portugal Primeira Liga",
+    "soccer_spain_la_liga": "La Liga",
+    "soccer_spain_segunda_division": "Spain Segunda Division",
+    "soccer_sweden_allsvenskan": "Sweden Allsvenskan",
+    "soccer_sweden_superettan": "Sweden Superettan",
+    "soccer_switzerland_superleague": "Switzerland Super League",
+    "soccer_turkey_super_league": "Turkey Super League",
+    "soccer_uefa_champs_league": "UEFA Champions League",
+    "soccer_uefa_champs_league_women": "UEFA Champions League Women",
+    "soccer_uefa_europa_conference_league": "UEFA Europa Conference League",
+    "soccer_uefa_europa_league": "UEFA Europa League",
+    "soccer_uefa_nations_league": "UEFA Nations League",
+    "soccer_usa_mls": "USA Major League Soccer"
+}
+
 def load_config():
     try:
         with open(CONFIG_FILE, 'r') as f:
@@ -256,11 +304,14 @@ def create_tip_file(match, action, template_file="prompt-examples/gpt-generated-
         logger.error("Failed to read template file %s: %s", template_file, e)
         return
 
+    # Get the human-readable league name
+    league_name = LEAGUE_NAMES.get(match['league'], match['league'])
+
     # Replace placeholders in the template with match details
     filled_content = template_content.format(
         team1=match['team1'],
         team2=match['team2'],
-        sport_title=match['league'],
+        sport_title=league_name,
         commence_time=match['commence_time']
     )
 
