d301dbb745d2a75342f643919d0230bced3bacaf
Andrei R Stamate <a144185i@gmail.com>
2026-01-11 11:48:52 +0200

    move to fly

---
+++ upload.py
@@ -0,0 +1,19 @@
+import requests
+import os
+
+API_KEY = os.environ["API_KEY"]
+
+def upload(folder, url):
+    for name in os.listdir(folder):
+        if name.endswith(".json"):
+            path = os.path.join(folder, name)
+            print(f"→ Uploading {name}")
+            with open(path, "rb") as f:
+                requests.post(
+                    url,
+                    headers={"X-API-KEY": API_KEY},
+                    files={"file": f},
+                )
+
+upload("/code/cache/football", "https://small-artifactory.fly.dev/football/upload")
+upload("/code/cache/basketball", "https://small-artifactory.fly.dev/basketball/upload")
